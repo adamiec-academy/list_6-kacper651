bd0e4a81f5ebbf088db810f110f0d9794c0fd00a
kacper651 <kacper.wroblewski651@gmail.com>
2023-02-22 13:38:52 +0100

    finished tasks 1 and 3

--- task_1.py
+++ task_1.py
@@ -1,5 +1,35 @@
+from copy import copy
 from random import shuffle
 
 
 def generate_assignments(previous_assignments, coders):
-    return {}
+    shuffled_coders = copy(coders)
+    while True:
+        shuffle(shuffled_coders)
+        new_assignments = {}
+        for i in range(len(coders)):
+            new_assignments[coders[i]] = shuffled_coders[i]
+        if are_assignments_correct(previous_assignments, new_assignments):
+            return new_assignments
+
+
+def are_assignments_correct(previous_assignments, new_assignments):
+    for coder in new_assignments:
+        # nie można być swoim własnym recenzentem
+        if coder == new_assignments[coder]:
+            return False
+        # nie można recenzować dwa razy z rzędu tej samej osoby
+        if new_assignments[coder] == previous_assignments[coder]:
+            return False
+        # nie można być recenzentem swojego recenzenta
+        if new_assignments[coder] == new_assignments[new_assignments[coder]]:
+            return False
+    return True
+
+
+coders = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+previous_assignments = {'A': 'B', 'B': 'C', 'C': 'D', 'D': 'E', 'E': 'F', 'F': 'G', 'G': 'H', 'H': 'I', 'I': 'J', 'J': 'A'}
+new_assignments = generate_assignments(previous_assignments, coders)
+print(new_assignments)
+new_assignments2 = generate_assignments(new_assignments, coders)
+print(new_assignments2)
--- task_3.py
+++ task_3.py
@@ -7,7 +7,7 @@ def info(data, turn_counter):
     print("-------- {} --------".format(turn_counter))
     for row in data:
         for element in row:
-            print(element, end="")
+            print(f"{element} ", end="")
         print()
 
 
@@ -15,29 +15,77 @@ def make_turn(board):
     new_board = deepcopy(board)
     size_y, size_x = len(board), len(board[0])
 
-    # TODO: Make a turn
-    
+    adj_cells_count = 0
+    for i in range(size_y):
+        for j in range(size_x):
+            neighbourhood = get_adj_cells(i, j, size_y, size_x)
+            for point in neighbourhood:
+                if board[point[0]][point[1]] == "X":
+                    adj_cells_count += 1
+            if adj_cells_count > 3 or adj_cells_count < 2:
+                new_board[i][j] = "."
+            elif adj_cells_count == 3:
+                new_board[i][j] = "X"
+
+            adj_cells_count = 0
+
     return new_board
 
 
-def play(starting_board, turns):
-    
-    current_board = starting_board
+def get_adj_cells(i, j, m, n):
+    adj_cells = []
+    if i > 0:
+        adj_cells.append((i - 1, j))
+    if i + 1 < m:
+        adj_cells.append((i + 1, j))
+    if j > 0:
+        adj_cells.append((i, j - 1))
+    if j + 1 < n:
+        adj_cells.append((i, j + 1))
+    if i > 0 and j > 0:
+        adj_cells.append((i - 1, j - 1))
+    if i + 1 < m and j + 1 < n:
+        adj_cells.append((i + 1, j + 1))
+    if i > 0 and j + 1 < n:
+        adj_cells.append((i - 1, j + 1))
+    if i + 1 < m and j > 0:
+        adj_cells.append((i + 1, j - 1))
 
-    for _ in range(turns):
+    return adj_cells
+
+
+def play(starting_board, turns):
+    current_board = starting_board
+    info(current_board, 0)
+
+    for i in range(turns):
+        sleep(1)
         current_board = make_turn(current_board)
+        info(current_board, i + 1)
 
     return current_board
 
 
 def play_forever(starting_board, interval):
-    
     current_board = starting_board
     current_turn = 0
     info(current_board, current_turn)
 
     while True:
+        sleep(interval)
         current_board = make_turn(current_board)
         current_turn += 1
         info(current_board, current_turn)
-        sleep(interval)
+
+
+board = [
+            [".", ".", ".", ".", ".", ".", ".", ],
+            [".", "X", ".", "X", "X", "X", ".", ],
+            [".", "X", ".", "X", ".", ".", ".", ],
+            [".", "X", "X", "X", "X", "X", ".", ],
+            [".", ".", ".", "X", ".", "X", ".", ],
+            [".", "X", "X", "X", ".", "X", ".", ],
+            [".", ".", ".", ".", ".", ".", ".", ],
+        ]
+
+play(board, 6)
